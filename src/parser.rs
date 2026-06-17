ea7292589cfba94055ca191cf8327b80fa1a7595
Arjun Manikandan <146555946+axm5507@users.noreply.github.com>
2026-06-17 12:33:00 -0500

    update parser to handle expressions and operations

--- src/parser.rs
+++ src/parser.rs
@@ -14,8 +14,10 @@ impl Parser {
 
         self.expect(TokenKind::LBrace)?;
         self.expect(TokenKind::ReturnKw)?;
-
-        let value = self.expect_number()?;
+        
+        //let value = self.expect_number()?;
+        //replacing this for version 2
+        let expr = self.parse_expr()?;
 
         self.expect(TokenKind::Semi)?;
         self.expect(TokenKind::RBrace)?;
@@ -24,8 +26,92 @@ impl Parser {
         Ok(Program {
             function: Function {
                 name,
-                body: Stmt::Return(Expr::Int(value)),
+                //body: Stmt::Return(Expr::Int(value)),
+                //I'm also replacing this for version 2
+                body: Stmt::Return(expr),
             },
         })
+    }
+    //now adding the parsing of different mathematical stuff for version 2
+    fn parse_expr(&mut self) -> Result<Expr, String>{
+        self.parse_additive()
+    }
+    // this is for addition and subtraction
+    fn parse_additive(&mut self) -> Result<Expr, String>{
+        let mut expr = self.parse_multiplicative()?;
+
+        loop {
+            if self.consume(TokenKind::Plus) {
+                expr = Expr::Binary(
+                    BinaryOp::Add,
+                    Box::new(expr),
+                    Box::new(self.parse_multiplicative()?),
+            );
+        }     else if self.consume(TokenKind::Minus) {
+                expr = Expr::Binary(
+                    BinaryOp::Sub,
+                    Box::new(expr),
+                    Box::new(self.parse_multiplicative()?),
+            );
+        }     else {
+                break;
+        }
+    }
+
+        Ok(expr)
+    }
+    //for multiplication, division, and modulus
+    fn parse_multiplicative(&mut self) -> Result<Expr, String>{
+        let mut expr = self.parse_unary()?;
+        loop{
+            if self.consume(TokenKind::Star){
+                expr = Expr::Binary(
+                    BinaryOp::Mul,
+                    Box::new(expr),
+                    Box::new(self.parse_unary()?),
+                    );
+            }
+            else if self.consume(TokenKind::Slash){
+                expr = Expr::Binary(
+                    BinaryOp::Div,
+                    Box::new(expr),
+                    Box::new(self.parse_unary()?),
+                    );
+            }
+            else if self.consume(TokenKind::Percent){
+                expr = Expr::Binary(
+                    BinaryOp::Mod,
+                    Box::new(expr),
+                    Box::new(self.parse_unary()?),
+                    );
+            }
+            else{
+                break;
+            }
+        }
+        Ok(expr)
+    
+    }
+    //for negative nums
+    fn parse_unary(&mut self) -> Result<Expr, String>{
+        if self.consume(TokenKind::Minus){
+            return Ok(Expr::Unary(
+                UnaryOp::Neg,
+                Box::new(self.parse_unary()?),
+                ));
+        }
+        self.parse_primary()
+    }
+    //for numbers and parenthesis
+    fn parse_primary(&mut self) -> Result<Expr, String>{
+        if self.consume(TokenKind::LParen){
+            let expr = self.parse_expr()?;
+            self.expect(TokenKind::RParen)?;
+            return Ok(expr);
+        }
+        if self.at(TokenKind::Number){
+            return Ok(Expr::Int(self.expect_number()?));
+        }
+        Err(self.error_here("expected expression"))
     }
 }
