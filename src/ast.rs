704a530b249d45ad2b83605f013166dddac52387
Arjun Manikandan <146555946+axm5507@users.noreply.github.com>
2026-06-17 12:00:24 -0500

    update AST to add unary and binary expressions

--- src/ast.rs
+++ src/ast.rs
@@ -17,4 +17,21 @@ pub enum Stmt{
 #[derive(Debug, Clone, PartialEq)]
 pub enum Expr{
     Int(i64),
+    //commit 2: now I'm gonna add unary and binary expressions so the compiler can do math/PEMDAS
+    Binary(BinaryOp, Box<Expr>, Box<Expr>),
+    Unary(UnaryOp, Box<Expr>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum BinaryOp{
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum UnaryOp{
+    Neg,
 }
